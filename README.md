45c619f9f73146d90b32bccd03f9902115e0a092
CHERVET Guillaume <guillaume.chervet@gmail.com>
2017-03-05 17:49:55 +0100

    http get sans body parser sans watch

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Missions
+# Cours API Rest
+
+expliquer node
+installer node via 
+https://github.com/coreybutler/nvm-windows
+https://github.com/creationix/nvm
+vérifier version de node
+lancer un script node hello world
+lancer un script fibonacci
+
+explication npm
+explication des version
+
+
+git tag -a v1.4 -m "http get sans body parser sans watch"
+
+
+
+
--- Sources/server.js
+++ Sources/server.js
@@ -1,51 +1,47 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+var users = require('./users.json')
+
+app.get('/users', function (request, response) {
+    response.end( JSON.stringify(users) );
+});
+
+/*
+app.post('/users', function (request, response) {
+    users.push(request.body);
+    res.end( JSON.stringify(data));
+});
+
+app.get('/users/:id', function (request, response) {
+   var id = request.params.id;
+    for(var i=0; i<users.length; i++){
+        var user = users[i];
+        if(user.id === id){
+            response.end(JSON.stringify(user));
+            break;
+        }
+   }
 })
 
-app.post('/addUser', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       data["user4"] = user["user4"];
-       console.log( data );
-       res.end( JSON.stringify(data));
-   });
-})
-
-app.get('/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
-   });
-})
-
-app.delete('/deleteUser', function (req, res) {
-
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       delete data["user" + 2];
-       
-       console.log( data );
-       res.end( JSON.stringify(data));
-   });
-})
+app.delete('/users/:id', function (request, response) {
+    var id = request.params.id;
+    for(var i=0; i<users.length; i++){
+        var user = users[i];
+        if(user.id === id){
+            var index = users.indexOf(5);
+            users.splice(index, 1);
+            response.end(JSON.stringify({}));
+            break;
+        }
+   }
+})*/
 
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log(`Example app listening at http://{host}:{port}`);
+  console.log(`Example app listening at http://${host}:${port}`);
 
 })
