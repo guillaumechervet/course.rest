4dd58450ee3d027a3fcbb8eef1cf13e56b4fa442
CHERVET Guillaume <guillaume.chervet@gmail.com>
2017-03-10 12:22:34 +0100

    maj

--- Sources/fibonacci.js
+++ Sources/fibonacci.js
@@ -19,3 +19,11 @@ console.log(`result is ${result}`);
 process.on('uncaughtException', (err) => {
     console.log(`Caught exception: ${err}`);
 });
+
+/*
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(fibonacci(40));
+}).listen(1337, "127.0.0.1");
+*/
