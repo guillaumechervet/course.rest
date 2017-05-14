9556c28d226b31ca2ace6188448eaf5baa3fd03d
CHERVET Guillaume <guillaume.chervet@gmail.com>
2017-05-14 17:07:13 +0200

    Mise à jour

--- Sources/app.js
+++ Sources/app.js
@@ -11,28 +11,33 @@ const app = express();
 app.use(fileUpload());
 
 app.post('/api/files', (request, response) => {
-    if (!request.files){
+    if (!request.files) {
         return request.status(400).send('No files were uploaded.');
     }
     let file = request.files.image;
     let id = uuidV1();
     var filename = id + '_' + file.name;
-    let filePath = path.join(__dirname, '/uploads', filename );
-    file.mv(filePath, function(err) {
-        if (err){
+    let filePath = path.join(__dirname, '/uploads', filename);
+    file.mv(filePath, function (err) {
+        if (err) {
             return response.status(500).send(err);
         }
         response.setHeader('Location', `/api/files/${filename}`);
-        response.json({id: id, filename: filename});
+        response.json({
+            id: id,
+            filename: filename
+        });
     });
 });
 
-app.get('/api/files/:filename', function(request, response){
+app.get('/api/files/:filename', function (request, response) {
     let filename = request.params.filename;
-    response.sendFile(path.join(__dirname,'/uploads', filename));
+    response.sendFile(path.join(__dirname, '/uploads', filename));
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var middlewareHttp = function (request, response, next) {
@@ -44,7 +49,9 @@ var middlewareHttp = function (request, response, next) {
         return;
     }
 
-    response.status(415).json({message:'Content-Type incorrect'});
+    response.status(415).json({
+        message: 'Content-Type incorrect'
+    });
 };
 app.use(middlewareHttp);
 
@@ -57,23 +64,29 @@ app.post('/api/places', function (request, response) {
     console.log('post /api/places called');
     let newPlace = Object.assign({}, request.body);
     let id = uuidV1();
-    newPlace.id = id; 
+    newPlace.id = id;
     _data.places.push(newPlace);
     response.setHeader('Location', `/api/places/${id}`);
-    response.json(newPlace);
+    response.status(204).json(newPlace);
 });
 
 app.put('/api/places/:id', function (request, response) {
     let id = request.params.id;
     console.log(`put /api/places/:id called with id ${id}`);
-    
+
     let places = _data.places;
-    let place = _.find(places, { 'id': id });
-    if(place === undefined) {
-        response.status(422).json({message:'Place not found'});
+    let place = _.find(places, {
+        'id': id
+    });
+    if (place === undefined) {
+        response.status(422).json({
+            message: 'Place not found'
+        });
         return;
     }
-    _.remove(places, { id: id });
+    _.remove(places, {
+        id: id
+    });
     places.push(request.body);
     response.status(204).json();
 });
@@ -83,10 +96,14 @@ app.patch('/api/places/:id', function (request, response) {
     console.log(`patch /api/places/:id called with id ${id}`);
 
     let places = _data.places;
-    let place = _.find(places, { 'id': id });
+    let place = _.find(places, {
+        'id': id
+    });
 
-    if(place === undefined) {
-        response.status(422).json({message:'Place not found'});
+    if (place === undefined) {
+        response.status(422).json({
+            message: 'Place not found'
+        });
         return;
     }
     Object.assign(place, request.body);
@@ -97,41 +114,42 @@ app.get('/api/places/:id', function (request, response) {
     let id = request.params.id;
     console.log(`get /api/places/:id called with id ${id}`);
     let places = _data.places;
-    let place = _.find(places, { 'id': id });
-    if(place !== undefined){
-        response.status(201).json(place);
+    let place = _.find(places, {
+        'id': id
+    });
+    if (place !== undefined) {
+        response.status(200).json(place);
         return;
     }
-    /*for(var i=0; i< places.length; i++){
-        var place = places[i];
-        if(place.id === id){
-            response.status(201).json(place);
-            break;
-        }
-    }*/
-    response.status(404).json({message:'Nothing found!'});
+    response.status(404).json({
+        message: 'Nothing found!'
+    });
 });
 
 app.delete('/api/places/:id', function (request, response) {
     var id = request.params.id;
     console.log(`delete /api/places/:id called with id ${id}`);
     var places = _data.places;
-    for(var i=0; i<places.length; i++){
-        var user = places[i];
-        if(user.id === id){
-            var index = places.indexOf(5);
-            places.splice(index, 1);
-            response.status(204).json();
-            return;
-        }
+    let place = _.find(places, {
+        'id': id
+    });
+    if (place !== undefined) {
+        var index = places.indexOf(place);
+        places.splice(index, 1);
+        response.status(204).json();
+        return;
     }
-    response.status(404).json({message:'Nothing found!'});
+    response.status(404).json({
+        message: 'Nothing found!'
+    });
 });
 
 // eslint-disable-next-line no-unused-vars
-app.use(function(error, request, response, next) {
+app.use(function (error, request, response, next) {
     console.error(error.stack);
-    response.status(500).json({message:'Something broke!'});
+    response.status(500).json({
+        message: 'Something broke!'
+    });
 });
 
 
