a661e187e082402c636b2c1d462a5c1f6d0d8c07
guillaume chervet <guillaume.chervet@gmail.com>
2017-05-17 18:35:50 +0200

    maj

--- Sources/files/controller.spec.js
+++ Sources/files/controller.spec.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 
-describe('Places/controller', () => {
+describe('Files/controller', () => {
 
-    it('GET /api/places should respond a http 200 OK', () => {
+  /*  it('GET /api/places should respond a http 200 OK', () => {
 
         const app = require('../app');
         request(app)
@@ -13,26 +13,6 @@ describe('Places/controller', () => {
                 if (err) throw err;
             });
 
-    });
+    });*/
 
-    it('DELETE /api/places/3 should respond a http 200 OK', () => {
-
-        const app = require('../app');
-        request(app)
-            .delete('/places')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) throw err;
-            });
-
-        request(app)
-            .delete('/places')
-            .expect('Content-Type', /json/)
-            .expect(404)
-            .end(function (err, res) {
-                if (err) throw err;
-            });
-
-    });
 });
