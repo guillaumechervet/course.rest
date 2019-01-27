94db747826cf6d6e59bb46b554f54cb221a5bff4
CHERVET Guillaume <guillaume.chervet@gmail.com>
2019-01-27 18:17:13 +0100

    maj

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const App = require('./app');
 
 const app = new App().app;
 
-var server = app.listen(8080, function() {
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
 
